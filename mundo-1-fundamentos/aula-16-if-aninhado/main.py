6ea890db09794c5c0572d70212b271cf32299bb9
JoselaineFer <103211487+JoselaineFer@users.noreply.github.com>
2026-07-27 19:06:31 -0300

    Create main.py

---
+++ mundo-1-fundamentos/aula-16-if-aninhado/main.py
@@ -0,0 +1,9 @@
+nota = float(input("Digite sua nota: "))
+
+   if nota >= 6:
+       if nota >= 9:
+           print("Aprovado com excelência!")
+       else:
+           print("Aprovado")
+   else:
+       print("Reprovado")
