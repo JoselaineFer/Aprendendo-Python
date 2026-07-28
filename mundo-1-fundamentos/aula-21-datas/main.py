b1326f4abb69995c0ccea77d2c70939247dfeb3e
JoselaineFer <103211487+JoselaineFer@users.noreply.github.com>
2026-07-27 21:17:28 -0300

    Create main.py

---
+++ mundo-1-fundamentos/aula-21-datas/main.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+
+   agora = datetime.now()
+   print("Data e hora agora:", agora)
+   print("Ano atual:", agora.year)
+   print("Dia da semana (0=segunda):", agora.weekday())
