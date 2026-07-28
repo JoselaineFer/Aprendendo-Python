db72a2c19ab9787791c47ec18c1fff855e9daa2c
JoselaineFer <103211487+JoselaineFer@users.noreply.github.com>
2026-07-27 21:20:04 -0300

    Create main.py

---
+++ mundo-1-fundamentos/aula-23-listas-avancado/main.py
@@ -0,0 +1,9 @@
+numeros = [5, 2, 8, 2, 1]
+
+   numeros.sort()
+   print("Ordenada:", numeros)
+
+   numeros.reverse()
+   print("Invertida:", numeros)
+
+   print("Quantas vezes o 2 aparece:", numeros.count(2))
