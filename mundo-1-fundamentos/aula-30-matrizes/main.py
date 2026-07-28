408d4690d55a1a8536a12c4b5e468957e4dd070e
JoselaineFer <103211487+JoselaineFer@users.noreply.github.com>
2026-07-28 11:46:09 -0300

    Create main.py

---
+++ mundo-1-fundamentos/aula-30-matrizes/main.py
@@ -0,0 +1,11 @@
+tabuleiro = [
+       ["x", "o", "x"],
+       ["o", "x", "o"],
+       ["x", "o", "x"]
+   ]
+
+   print(tabuleiro[0])
+   print(tabuleiro[0][1])
+
+   for linha in tabuleiro:
+       print(linha)
