1b39bdcc325f9b5ac409b7434fb6a6a1a9cc64ec
JoselaineFer <103211487+JoselaineFer@users.noreply.github.com>
2026-07-27 21:52:58 -0300

    Create main.py

---
+++ mundo-1-fundamentos/aula-28-list-comprehension/main.py
@@ -0,0 +1,7 @@
+numeros = [1, 2, 3, 4, 5, 6]
+
+   pares = [n for n in numeros if n % 2 == 0]
+   print("Números pares:", pares)
+
+   dobrados = [n * 2 for n in numeros]
+   print("Números dobrados:", dobrados)
