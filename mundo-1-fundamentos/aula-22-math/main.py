2495740d8c856c021294c971b46cc6d96b13db89
JoselaineFer <103211487+JoselaineFer@users.noreply.github.com>
2026-07-27 21:19:06 -0300

    Create main.py

---
+++ mundo-1-fundamentos/aula-22-math/main.py
@@ -0,0 +1,7 @@
+import math
+
+   numero = 16
+
+   print("Raiz quadrada:", math.sqrt(numero))
+   print("Arredondando pra cima:", math.ceil(3.2))
+   print("Arredondando pra baixo:", math.floor(3.8))
