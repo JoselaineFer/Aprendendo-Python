4b53c050cbe37da3d2ca16496fd6501f38106cb4
JoselaineFer <103211487+JoselaineFer@users.noreply.github.com>
2026-07-28 21:09:01 -0300

    Create main.py

---
+++ mundo-1-fundamentos/aula-40-strings-avancado/main.py
@@ -0,0 +1,8 @@
+frase = "Aprendendo Python é muito bom"
+
+   print(frase[::-1])
+
+   print("Python" in frase)
+
+   posicao = frase.find("Python")
+   print("A palavra 'Python' começa na posição:", posicao)
