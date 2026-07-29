566e71a4fc625a8ca156fc399c84709bd78092ee
JoselaineFer <103211487+JoselaineFer@users.noreply.github.com>
2026-07-29 09:34:08 -0300

    Create main.py

---
+++ mundo-1-fundamentos/aula-65-introspeccao/main.py
@@ -0,0 +1,10 @@
+idade = 31
+   nome = "Josi"
+   notas = [7, 8, 9]
+
+   print(type(idade))
+   print(type(nome))
+   print(type(notas))
+
+   print(isinstance(idade, int))
+   print(isinstance(nome, str))
