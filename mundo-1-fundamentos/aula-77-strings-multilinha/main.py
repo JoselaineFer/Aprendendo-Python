12d95fe44a27a35de8dfca961906f83db09b8629
JoselaineFer <103211487+JoselaineFer@users.noreply.github.com>
2026-07-29 14:55:17 -0300

    Create main.py

---
+++ mundo-1-fundamentos/aula-77-strings-multilinha/main.py
@@ -0,0 +1,14 @@
+nome = "Josi"
+   total = 150.00
+
+   recibo = f"""
+   ========================
+           RECIBO
+   ========================
+   Cliente: {nome}
+   Total: R$ {total:.2f}
+   ========================
+   Obrigado pela preferência!
+   """
+
+   print(recibo)
